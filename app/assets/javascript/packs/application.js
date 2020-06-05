b8f7472358f4d17d341f5e302b549d9c932fe899
kamilpikula <pikusix1@o2.pl>
2020-06-05 23:52:32 +0200

    delete turbolinks

--- app/assets/javascript/packs/application.js
+++ app/assets/javascript/packs/application.js
@@ -4,10 +4,8 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,7 +15,4 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap'
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+import "../packs/custom/bank_accounts_show"
