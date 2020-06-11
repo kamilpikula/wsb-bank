678e98683ff0c1962d6e8e6e95479e659adefcff
kamilpikula <pikusix1@o2.pl>
2020-06-11 19:27:53 +0200

    new clients, forms for this, nested bank accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  
   resources :bank_accounts
+  resources :clients do
+    resources :bank_accounts, controller: "clients/bank_accounts", except: [:index, :show, :destroy]
+  end
 
   namespace :api do
     namespace :v1 do
