1513b9150d5f32a47240c125102bf6bb66e49b73
kamilpikula <pikusix1@o2.pl>
2020-06-16 14:03:59 +0200

    refactor of index page, add helpers to dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def balance_of_user
+    number_to_currency(BankAccount.where(id: current_user.id).last.balance, :unit => "")
+  end
+
+  def transactions_count
+    AccountTransaction.where(bank_account_id: current_user.id).count
+  end
+
+  def last_transaction
+    if AccountTransaction.where(bank_account_id: current_user.id).count > 1
+      number_to_currency(AccountTransaction.where(bank_account_id: current_user.id).last.amount, :unit => "")
+    else
+      "0.00"
+    end
+  end
+
+  def user_sign_in_count
+    User.where(id: current_user.id).last.sign_in_count
+  end
+
 end
